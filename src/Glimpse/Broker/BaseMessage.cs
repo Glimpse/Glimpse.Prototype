09d1be86813f377219249559858c476a7c8d3aad
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-19 14:37:43 -0800

    Base message support for Broker

---
+++ src/Glimpse/Broker/BaseMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Glimpse.Broker
+{
+    public class BaseMessage : IMessage
+    {
+        public BaseMessage()
+        {
+            Id = new Guid();
+            Time = new DateTime();
+        }
+
+        public Guid Id { get; set; }
+
+        public DateTime Time { get; set; }
+    }
+}
---
+++ src/Glimpse/Broker/IMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Glimpse.Broker
+{
+    public interface IMessage
+    {
+        Guid Id { get; set; }
+
+        DateTime Time { get; set; }
+    }
+}
