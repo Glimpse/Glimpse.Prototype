7059476dacbbddb61ba4a40f5acf2d9f016b0555
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-19 14:48:23 -0800

    Add agent to the web middleware

--- src/Glimpse.Host.Web.AspNet/GlimpseMiddleware.cs
+++ src/Glimpse.Host.Web.AspNet/GlimpseMiddleware.cs
@@ -1,23 +1,30 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Glimpse.Host.Web.AspNet.Framework;
+using Glimpse.Agent.Web;
 
 namespace Glimpse.Host.Web.AspNet
 {
     public class GlimpseMiddleware
     {
         private readonly RequestDelegate _innerNext;
+        private readonly WebAgentRuntime _runtime;
 
         public GlimpseMiddleware(RequestDelegate innerNext)
         {
             _innerNext = innerNext;
+            _runtime = new WebAgentRuntime();   // TODO: This shouldn't have this direct depedency
         }
 
         public async Task Invoke(Microsoft.AspNet.Http.HttpContext context)
         {
             var newContext = new HttpContext(context);
 
+            _runtime.Begin(newContext);
+
             await _innerNext(context);
+
+            _runtime.End(newContext);
         }
     }
 }
