59dd401b7a25a53fdf294568a9742dea66f0d566
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-19 14:48:47 -0800

    Add agent to web owin middleware

--- src/Glimpse.Host.Web.Owin/GlimpseMiddleware.cs
+++ src/Glimpse.Host.Web.Owin/GlimpseMiddleware.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Glimpse.Host.Web.Owin.Framework;
+using Glimpse.Agent.Web;
 
 namespace Glimpse.Host.Web.Owin
 {
     public class GlimpseMiddleware
     {
         private readonly Func<IDictionary<string, object>, Task> _innerNext;
+        private readonly WebAgentRuntime _runtime;
 
         public GlimpseMiddleware(Func<IDictionary<string, object>, Task> innerNext)
         {
             _innerNext = innerNext;
+            _runtime = new WebAgentRuntime();   // TODO: This shouldn't have this direct depedency
         }
 
         public async Task Invoke(IDictionary<string, object> environment)
         {
             var newContext = new HttpContext(environment);
 
+            _runtime.Begin(newContext);
+
             await _innerNext(environment);
+
+            _runtime.End(newContext);
         }
     }
 }
