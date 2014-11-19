980a4394398232242170140c3d4f67093ad4c0c7
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-19 14:33:30 -0800

    Add Discoverable Collection
    Leverages discoverable type support

---
+++ src/Glimpse/IDiscoverableCollection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Glimpse
+{
+    public interface IDiscoverableCollection<T> : ICollection<T>
+    {    
+        void Discover();
+    }
+}
---
+++ src/Glimpse/Reflection/ReflectionDiscoverableCollection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Glimpse.Reflection
+{
+    public class ReflectionDiscoverableCollection<T> : List<T>, IDiscoverableCollection<T>
+    {
+        private readonly ITypeService _typeService;
+
+        public ReflectionDiscoverableCollection(ITypeService typeService)
+        {
+            _typeService = typeService;
+            CoreLibarary = "Glimpse";
+        }
+
+        public string CoreLibarary { get; set; }
+
+        public void Discover()
+        {
+            var instances = _typeService.Resolve<T>(CoreLibarary);
+
+            AddRange(instances);
+        }
+    }
+}
