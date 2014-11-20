6bbca6c90de1be02b59d11ea2309a3425e3f6a5f
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-19 20:21:12 -0800

    Add new RequestRuntime
    Prevent hosts from directly depending on the web agent

---
+++ src/Glimpse.Web.Common/Framework/RequestRuntime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Glimpse.Web
+{
+    public class RequestRuntime
+    {
+        public void Begin(IContext newContext)
+        {
+
+        }
+
+        public void End(IContext newContext)
+        {
+
+        }
+    }
+}
