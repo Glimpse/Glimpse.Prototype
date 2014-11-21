5cfaaac20c5ba03e10dd5bcdbd0a23e05a55216b
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-20 20:26:39 -0800

    Setup WebAgentRuntime as a target runtime

--- src/Glimpse.Agent.Web/WebAgentRuntime.cs
+++ src/Glimpse.Agent.Web/WebAgentRuntime.cs
@@ -3,16 +3,20 @@ using System;
 
 namespace Glimpse.Agent.Web
 {
-    public class WebAgentRuntime
+    public class WebAgentRuntime : IRequestRuntime
     {
         public void Begin(IContext newContext)
         {
+            var message = new BeginRequestMessage();
 
+            // TODO: Publish messages 
         }
 
         public void End(IContext newContext)
         {
+            var message = new EndRequestMessage();
 
+            // TODO: Publish messages 
         }
     }
 }
