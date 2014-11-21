c68ca95b41ca9a6187ea13151165b0aa55080ad1
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-21 14:13:04 -0800

    Introduce RequestHandler to allow resources to be served

---
+++ src/Glimpse.Web.Common/Framework/IRequestHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Glimpse.Web
+{
+    public interface IRequestHandler
+    {
+        bool WillHandle(IContext context);
+
+        Task Handle(IContext context);
+    }
+}
--- src/Glimpse.Web.Common/Framework/MasterRequestRuntime.cs
+++ src/Glimpse.Web.Common/Framework/MasterRequestRuntime.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Framework.DependencyInjection;
+using System.Threading.Tasks;
 
 namespace Glimpse.Web
 {
     public class MasterRequestRuntime
     {
         private readonly IDiscoverableCollection<IRequestRuntime> _requestRuntimes;
+        private readonly IDiscoverableCollection<IRequestHandler> _requestHandlers;
 
         public MasterRequestRuntime(IServiceProvider serviceProvider)
         {
             _requestRuntimes = serviceProvider.GetService<IDiscoverableCollection<IRequestRuntime>>();
             _requestRuntimes.Discover();
+
+            _requestHandlers = serviceProvider.GetService<IDiscoverableCollection<IRequestHandler>>();
+            _requestHandlers.Discover();
         }
 
         public void Begin(IContext context)
@@ -20,6 +25,22 @@ namespace Glimpse.Web
             {
                 requestRuntime.Begin(context);
             }
+        }
+
+
+        public bool TryGetHandle(IContext context, out IRequestHandler handeler)
+        {
+            foreach (var requestHandler in _requestHandlers)
+            {
+                if (requestHandler.WillHandle(context))
+                {
+                    handeler = requestHandler;
+                    return true;
+                }
+            }
+
+            handeler = null;
+            return false;
         }
 
         public void End(IContext context)
