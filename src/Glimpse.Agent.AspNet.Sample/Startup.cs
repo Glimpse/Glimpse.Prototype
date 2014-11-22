0acca16509f2e6358a115789bff82eb51b97c16f
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-21 16:18:14 -0800

    Create base new sample projects

---
+++ src/Glimpse.Agent.AspNet.Sample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace Glimpse.Agent.AspNet.Sample
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        }
+    }
+}
