77874f7cb81eefb599e4ad5ee8ce29d685da1fcc
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-23 13:58:29 -0800

    Add stub implementation of local

---
+++ src/Glimpse.Server.Web/Broker/LocalBrokerPublisher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Glimpse.Server
+{
+    public class LocalBrokerPublisher : IBrokerPublisher
+    {
+        public void PublishMessage(IMessage message)
+        {
+            // TODO: push straight to the bus
+        }
+    }
+}
