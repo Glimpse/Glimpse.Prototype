68272a70e1e4106b89ae82fd696a4d2534d0a08e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-23 12:00:17 -0800

    Added stub implmentation for stream

---
+++ src/Glimpse.Agent.Connection.Stream/RemoteStreamBrokerPublisher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Glimpse.Agent
+{
+    public class RemoteStreamBrokerPublisher : IBrokerPublisher
+    {
+        public void PublishMessage(IMessage message)
+        {
+            // TODO: Use SignalR to publish message
+        }
+    }
+}
