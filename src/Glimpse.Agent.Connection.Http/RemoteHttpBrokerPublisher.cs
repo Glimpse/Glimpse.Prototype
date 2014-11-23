f2cdb875a3a06fe2dd1533c69c24963437d54088
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-23 12:00:27 -0800

    Add stub implementation for http

---
+++ src/Glimpse.Agent.Connection.Http/RemoteHttpBrokerPublisher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Glimpse.Agent
+{
+    public class RemoteHttpBrokerPublisher : IBrokerPublisher
+    {
+        public void PublishMessage(IMessage message)
+        {
+            // TODO: Use http client to publish messages
+        }
+    }
+}
