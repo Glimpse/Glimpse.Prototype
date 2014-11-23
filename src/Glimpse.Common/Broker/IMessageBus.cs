6846d469bb3a6e7056068c1e224f9e6fdd87f854
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-23 14:42:00 -0800

    Add initial contract for message bus
    Ya observables!!

---
+++ src/Glimpse.Common/Broker/IMessageBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Glimpse.Broker
+{
+    public interface IMessageBus
+    {
+        IObservable<T> Listen<T>();
+
+        IObservable<T> ListenIncludeLatest<T>();
+
+        bool IsRegistered(Type type);
+    }
+}
