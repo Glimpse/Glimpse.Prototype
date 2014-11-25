f87204acc1c636f6c06e660f78a6b1402604c970
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-25 15:27:56 -0800

    Add base http message publisher

--- src/Glimpse.Agent.Connection.Http/Broker/RemoteHttpMessagePublisher.cs
+++ src/Glimpse.Agent.Connection.Http/Broker/RemoteHttpMessagePublisher.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace Glimpse.Agent
 {
-    public class RemoteHttpMessagePublisher : IMessagePublisher
+    public class RemoteHttpMessagePublisher : IMessagePublisher, IDisposable
     {
+        private readonly HttpClient _httpClient;
+        private readonly HttpClientHandler _httpHandler;
+
+        public RemoteHttpMessagePublisher()
+        {
+            _httpHandler = new HttpClientHandler();
+            _httpClient = new HttpClient(_httpHandler);
+        }
+
         public void PublishMessage(IMessage message)
         {
-            // TODO: Use http client to publish messages
+            var content = new StringContent("Hello");
+
+
+            // TODO: Try shifting to async and await
+            _httpClient.PostAsync("http://localhost:15999/Glimpse/Agent", content)
+                .ContinueWith(requestTask =>
+                    {
+                        // Get HTTP response from completed task.
+                        HttpResponseMessage response = requestTask.Result;
+
+                        // Check that response was successful or throw exception
+                        response.EnsureSuccessStatusCode();
+
+                        // Read response asynchronously as JsonValue and write out top facts for each country
+                        var result = response.Content.ReadAsStringAsync().Result;
+
+                        /*
+                        response.Content.ReadAsAsync<string>().ContinueWith(readTask =>
+                            {
+                                var result = readTask.Result;
+                            });
+                            */
+                    });
+        }
+
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+            _httpHandler.Dispose();
         }
     }
 }
