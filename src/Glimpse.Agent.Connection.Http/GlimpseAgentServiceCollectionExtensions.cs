49183c8a3723b6e1317bb1e6dc4243d62c11b36c
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-25 15:29:20 -0800

    Add hocks for people registering the http specific services

---
+++ src/Glimpse.Agent.Connection.Http/GlimpseAgentServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using System;
+
+namespace Glimpse
+{
+    public static class GlimpseAgentServiceCollectionExtensions
+    {
+        public static IServiceCollection WithRemoteHttpAgent(this IServiceCollection services)
+        {
+            return services.Add(GlimpseAgentServices.GetDefaultServices());
+        }
+
+        public static IServiceCollection WithRemoteHttpAgent(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.Add(GlimpseAgentServices.GetDefaultServices(configuration));
+        }
+    }
+}
