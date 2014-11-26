af6212bf9827798499ebf72f2aacfeee12d50df6
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-26 12:27:33 -0800

    Add in MessageAgentBus definition

---
+++ src/Glimpse.Agent.Common/Framework/IMessageAgentBus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Glimpse.Agent
+{
+    public interface IMessageAgentBus
+    {
+        IObservable<T> Listen<T>()
+            where T : IMessage;
+
+        IObservable<T> ListenIncludeLatest<T>()
+            where T : IMessage;
+
+        void SendMessage(IMessage message);
+    }
+}
