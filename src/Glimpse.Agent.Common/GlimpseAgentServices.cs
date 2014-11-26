9608fdf9f8eb0c59d0be10ba98dbec8701df6e49
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-26 14:36:11 -0800

    Allow agent services to be registered

---
+++ src/Glimpse.Agent.Common/GlimpseAgentServices.cs
@@ -0,0 +1,26 @@
+﻿using Glimpse.Agent;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace Glimpse
+{
+    public class GlimpseAgentServices
+    {
+        public static IEnumerable<IServiceDescriptor> GetDefaultServices()
+        {
+            return GetDefaultServices(new Configuration());
+        }
+
+        public static IEnumerable<IServiceDescriptor> GetDefaultServices(IConfiguration configuration)
+        {
+            var describe = new ServiceDescriber(configuration);
+
+            //
+            // Broker
+            //
+            yield return describe.Singleton<IMessageAgentBus, DefaultMessageAgentBus>();
+        }
+    }
+}
