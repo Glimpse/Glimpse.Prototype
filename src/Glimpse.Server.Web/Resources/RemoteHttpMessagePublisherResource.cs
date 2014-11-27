d09958cfb437c5fedd4b028c67c101e71f76dd66
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-26 16:40:54 -0800

    Get http conn resource to read json and publish to server bus

--- src/Glimpse.Server.Web/Resources/RemoteHttpMessagePublisherResource.cs
+++ src/Glimpse.Server.Web/Resources/RemoteHttpMessagePublisherResource.cs
@@ -1,4 +1,5 @@
 ﻿using Glimpse.Web;
+using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Text;
@@ -8,6 +9,13 @@ namespace Glimpse.Server.Resources
 {
     public class RemoteHttpMessagePublisherResource : IRequestHandler
     {
+        private readonly IMessageServerBus _messageServerBus;
+
+        public RemoteHttpMessagePublisherResource(IMessageServerBus messageServerBus)
+        {
+            _messageServerBus = messageServerBus;
+        }
+
         public bool WillHandle(IContext context)
         {
             return context.Request.Path.StartsWith("/Glimpse/Agent");
@@ -15,18 +23,28 @@ namespace Glimpse.Server.Resources
 
         public async Task Handle(IContext context)
         {
-            // TEST CODE ONLY!!!!
-            var request = context.Request;
-            var reader = new StreamReader(request.Body);
-            var text = reader.ReadToEnd();
+            var envelope = ReadMessage(context.Request);
 
+            _messageServerBus.SendMessage(envelope);
+
+            // TEST CODE ONLY!!!!
             var response = context.Response;
 
             response.SetHeader("Content-Type", "text/plain");
 
-            var data = Encoding.UTF8.GetBytes(text);
+            var data = Encoding.UTF8.GetBytes(envelope.Message);
             await response.WriteAsync(data);
             // TEST CODE ONLY!!!!
+        }
+
+        private MessageEnvelope ReadMessage(IHttpRequest request)
+        {
+            var reader = new StreamReader(request.Body);
+            var text = reader.ReadToEnd();
+
+            var message = JsonConvert.DeserializeObject<MessageEnvelope>(text);
+
+            return message;
         }
     }
 }
