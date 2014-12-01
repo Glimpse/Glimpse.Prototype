e2749e8d57156cbb9649e9814eefb8577179bdb5
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-11-30 17:26:43 -0800

    Get hub endpoint on the server side to receive messages

---
+++ src/Glimpse.Server.Web/Resources/RemoteStreamMessagePublisherResource.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+
+namespace Glimpse.Server.Resources
+{
+    public class RemoteStreamMessagePublisherResource : Hub // Temp dont want this to be public
+    {
+        private readonly IMessageServerBus _messageServerBus;
+
+        public RemoteStreamMessagePublisherResource(IMessageServerBus messageServerBus)
+        {
+            _messageServerBus = messageServerBus;
+        }
+
+        public void HandleMessage(MessageEnvelope envelope)
+        {
+            _messageServerBus.SendMessage(envelope); 
+        }
+    }
+}
