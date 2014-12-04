92f220282eb96755dac943bf3aa409daa18c7897
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-12-03 20:03:27 -0500

    Add stub client project to finish the pipe connection
    From agent to server and server to client

---
+++ src/Glimpse.Client.Web/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+
+namespace Glimpse.Client.Web
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+            app.UseStaticFiles();
+        }
+    }
+}
