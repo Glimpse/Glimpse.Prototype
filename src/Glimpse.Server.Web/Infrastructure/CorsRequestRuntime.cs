63adf5c92660ae2ee55f40af35c1dc9a1347d655
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-12-06 16:07:42 -0500

    Quick and dirty implementation for cors support

---
+++ src/Glimpse.Server.Web/Infrastructure/CorsRequestRuntime.cs
@@ -0,0 +1,18 @@
+﻿using Glimpse.Web;
+using System;
+using System.Threading.Tasks;
+
+namespace Glimpse.Server.Infrastructure
+{
+    public class CorsRequestRuntime : IRequestRuntime
+    {
+        public async Task Begin(IContext newContext)
+        {
+            newContext.Response.SetHeader("Access-Control-Allow-Origin", "*");
+        }
+
+        public async Task End(IContext newContext)
+        {
+        }
+    }
+}
