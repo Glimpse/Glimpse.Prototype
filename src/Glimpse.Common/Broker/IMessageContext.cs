a6db74ffe2f39d459c80ee437d4ba4a00ace49c1
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-12-29 19:52:08 +1000

    Switch back to interface

---
+++ src/Glimpse.Common/Broker/IMessageContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Glimpse.Broker
+{
+    // TODO: currently an interface as I'm not sure if different
+    //       agent types may want to add extra properties 
+    public interface IMessageContext
+    {
+        Guid Id { get; set; }
+
+        string Type { get; set; }
+    }
+}
