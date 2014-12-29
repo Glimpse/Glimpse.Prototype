79039a1bcc0d9b8c1403812e139907bf93884256
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2014-12-29 19:48:09 +1000

    Add note about the interface

--- src/Glimpse.Common/Broker/IMessageContext.cs
+++ src/Glimpse.Common/Broker/IMessageContext.cs
@@ -2,6 +2,8 @@
 
 namespace Glimpse.Broker
 {
+    // TODO: currently an interface as I'm not sure if different
+    //       agent types may want to add extra properties
     public interface IMessageContext
     {
         Guid Id { get; set; }
