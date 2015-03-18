6615cb2c9c84f18b5a623f11f1464d98f593f65e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-18 14:01:53 -0700

    Add contenttype descriptors

---
+++ src/Glimpse.Agent.Web/Options/IgnoreContentTypeDescriptor.cs
@@ -0,0 +1,12 @@
+﻿namespace Glimpse.Agent.Web.Options
+{
+    public class IgnoreContentTypeDescriptor
+    {
+        public IgnoreContentTypeDescriptor(string contentType)
+        {
+            ContentType = contentType;
+        }
+
+        public string ContentType { get; }
+    }
+}
---
+++ src/Glimpse.Agent.Web/Options/IgnoreContentTypeDescriptorExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Glimpse.Agent.Web.Options
+{
+    public static class IgnoreContentTypeDescriptorExtensions
+    {
+        public static IgnoreContentTypeDescriptor Add(this IList<IgnoreContentTypeDescriptor> descriptors, string contentType)
+        {
+            var descriptor = new IgnoreContentTypeDescriptor(contentType);
+            descriptors.Add(descriptor);
+            return descriptor;
+        }
+
+        public static IgnoreContentTypeDescriptor Insert(this IList<IgnoreContentTypeDescriptor> descriptors, int index, string contentType)
+        {
+            if (index < 0 || index > descriptors.Count)
+            {
+                throw new ArgumentOutOfRangeException("index");
+            }
+
+            var descriptor = new IgnoreContentTypeDescriptor(contentType);
+            descriptors.Insert(index, descriptor);
+            return descriptor;
+        }
+    }
+}
