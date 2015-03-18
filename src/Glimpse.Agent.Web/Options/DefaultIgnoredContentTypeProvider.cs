7d72d33b44aa2b9722e687b943b54eb5db8e01a7
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-18 14:19:03 -0700

    Add provider for processing ContentType

---
+++ src/Glimpse.Agent.Web/Options/DefaultIgnoredContentTypeProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Framework.OptionsModel;
+
+namespace Glimpse.Agent.Web.Options
+{ 
+    public class DefaultIgnoredContentTypeProvider : IIgnoredContentTypeProvider
+    {
+        public DefaultIgnoredContentTypeProvider(IOptions<GlimpseAgentWebOptions> optionsAccessor)
+        {
+            var contentTypes = optionsAccessor.Options.ContentTypes;
+            ContentTypes = contentTypes.ToList();
+        }
+
+        public IReadOnlyList<IgnoredContentTypeDescriptor> ContentTypes { get; }
+    }
+}
---
+++ src/Glimpse.Agent.Web/Options/IIgnoredContentTypeProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Glimpse.Agent.Web.Options
+{
+    public interface IIgnoredContentTypeProvider
+    {
+        IReadOnlyList<IgnoredContentTypeDescriptor> ContentTypes { get; }
+    }
+}
