6ed6cf03885674422fb033084ede707bec8735bd
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-18 10:51:59 -0700

    Add point for registering agent web services

---
+++ src/Glimpse.Agent.Web/GlimpseAgentWebServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using System;
+
+namespace Glimpse
+{
+    public static class GlimpseAgentWebServiceCollectionExtensions
+    {
+        public static IServiceCollection ForWeb(this IServiceCollection services)
+        {
+            return services.Add(GlimpseAgentWebServices.GetDefaultServices());
+        }
+
+        public static IServiceCollection ForWeb(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.Add(GlimpseAgentWebServices.GetDefaultServices(configuration));
+        }
+    }
+}
---
+++ src/Glimpse.Agent.Web/GlimpseAgentWebServices.cs
@@ -0,0 +1,27 @@
+﻿using Glimpse.Agent;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using Glimpse.Agent.Web.Options;
+
+namespace Glimpse
+{
+    public class GlimpseAgentWebServices
+    {
+        public static IEnumerable<IServiceDescriptor> GetDefaultServices()
+        {
+            return GetDefaultServices(new Configuration());
+        }
+
+        public static IEnumerable<IServiceDescriptor> GetDefaultServices(IConfiguration configuration)
+        {
+            var describe = new ServiceDescriber(configuration);
+
+            //
+            // Options
+            //
+            yield return describe.Singleton<IIgnoredUrisProvider, DefaultIgnoredUrisProvider>();
+        }
+    }
+}
