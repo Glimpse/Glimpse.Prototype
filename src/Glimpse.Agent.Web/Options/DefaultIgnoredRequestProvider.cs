fe1e491da1dceffd01c54fa81b37b353e8e4e95d
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-20 18:51:13 -0700

    Add default ignored request provider which discovers instances

---
+++ src/Glimpse.Agent.Web/Options/DefaultIgnoredRequestProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Messaging;
+
+namespace Glimpse.Agent.Web.Options
+{
+    public class DefaultIgnoredRequestProvider : IIgnoredRequestProvider
+    {
+        private readonly ITypeService _typeService;
+
+        public DefaultIgnoredRequestProvider(ITypeService typeService)
+        { 
+        }
+
+        public IEnumerable<IIgnoredRequestPolicy> Policies
+        {
+            get { return _typeService.Resolve<IIgnoredRequestPolicy>().ToArray(); }
+        }
+    }
+}
