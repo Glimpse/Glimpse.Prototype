cccf9b3b5426f207b41e59d3807a77aa7d5ee3c1
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-20 18:50:26 -0700

    Add ignored request provider

---
+++ src/Glimpse.Agent.Web/Options/IIgnoredRequestProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Glimpse.Agent.Web.Options
+{
+    public interface IIgnoredRequestProvider
+    {
+        IEnumerable<IIgnoredRequestPolicy> Policies { get; }
+    }
+}
