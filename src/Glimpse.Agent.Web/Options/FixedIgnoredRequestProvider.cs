0e31568d5719ab18240f5fba39f2d4bad9a953e8
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-20 18:51:37 -0700

    Add fixed ignored request provider to allow user full controll

---
+++ src/Glimpse.Agent.Web/Options/FixedIgnoredRequestProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Glimpse.Agent.Web.Options
+{
+    public class FixedIgnoredRequestProvider : IIgnoredRequestProvider
+    {
+        public FixedIgnoredRequestProvider()
+            : this(Enumerable.Empty<IIgnoredRequestPolicy>())
+        {
+        }
+
+        public FixedIgnoredRequestProvider(IEnumerable<IIgnoredRequestPolicy> controllerTypes)
+        {
+            Policies = new List<IIgnoredRequestPolicy>(controllerTypes);
+        }
+        
+        public IList<IIgnoredRequestPolicy> Policies { get; }
+
+        IEnumerable<IIgnoredRequestPolicy> IIgnoredRequestProvider.Policies => Policies;
+    }
+}
