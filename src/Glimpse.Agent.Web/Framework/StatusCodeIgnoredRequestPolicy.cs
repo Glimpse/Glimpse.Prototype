4aed552350a3e5ea4a87a2058e550b47cb8535dd
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-20 19:27:55 -0700

    Status Code request policy

---
+++ src/Glimpse.Agent.Web/Framework/StatusCodeIgnoredRequestPolicy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Glimpse.Web;
+using Microsoft.Framework.OptionsModel;
+
+namespace Glimpse.Agent.Web.Framework
+{
+    public class StatusCodeIgnoredRequestPolicy : IIgnoredRequestPolicy
+    {
+        private readonly IList<int> _statusCodes;
+
+        public StatusCodeIgnoredRequestPolicy(IOptions<GlimpseAgentWebOptions> optionsAccessor)
+        {
+            _statusCodes = optionsAccessor.Options.StatusCodes;
+        }
+
+        public bool ShouldIgnore(IHttpContext context)
+        {
+            return _statusCodes.Contains(context.Response.StatusCode);
+        }
+    }
+}
