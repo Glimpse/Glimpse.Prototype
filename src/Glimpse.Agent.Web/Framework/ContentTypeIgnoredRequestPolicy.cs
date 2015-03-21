f3d1659f90499e18491a4af5fccae1ede20f3f83
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-20 19:27:29 -0700

    Content Type request policy

---
+++ src/Glimpse.Agent.Web/Framework/ContentTypeIgnoredRequestPolicy.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using Glimpse.Web;
+using Microsoft.Framework.OptionsModel;
+
+namespace Glimpse.Agent.Web.Framework
+{
+    public class ContentTypeIgnoredRequestPolicy : IIgnoredRequestPolicy
+    {
+        private readonly IList<string> _contextType;
+
+        public ContentTypeIgnoredRequestPolicy(IOptions<GlimpseAgentWebOptions> optionsAccessor)
+        {
+            _contextType = optionsAccessor.Options.ContentTypes;
+        }
+
+        public bool ShouldIgnore(IHttpContext context)
+        {
+            return _contextType.Contains(context.Response.ContentType);
+        }
+    }
+}
