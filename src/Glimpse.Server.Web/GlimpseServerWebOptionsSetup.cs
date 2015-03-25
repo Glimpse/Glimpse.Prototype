6b72f7ebe7724ad741ccd33bdcd4b12a67dfaa55
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-25 12:45:53 -0700

    Add Server Web Options Setup

---
+++ src/Glimpse.Server.Web/GlimpseServerWebOptionsSetup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Framework.OptionsModel;
+
+namespace Glimpse.Server
+{
+    public class GlimpseServerWebOptionsSetup : ConfigureOptions<GlimpseServerWebOptions>
+    {
+        public GlimpseServerWebOptionsSetup() : base(ConfigureGlimpseServerWebOptions)
+        {
+            Order = -1000;
+        }
+
+        public static void ConfigureGlimpseServerWebOptions(GlimpseServerWebOptions options)
+        {
+            // TODO: Setup different settings here
+        }
+    }
+}
