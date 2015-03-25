2550189c5dfa2bb6e145f944599c17ae55ae461e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-25 16:10:30 -0700

    Add policy for local only requests

---
+++ src/Glimpse.Server.Web/Framework/AllowRemoteSecureRequestPolicy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Glimpse.Server.Options;
+using Glimpse.Web;
+
+namespace Glimpse.Server.Framework
+{
+    public class AllowRemoteSecureRequestPolicy : ISecureRequestPolicy
+    {
+        private readonly IAllowRemoteProvider _allowRemoteProvider;
+
+        public AllowRemoteSecureRequestPolicy(IAllowRemoteProvider allowRemoteProvider)
+        {
+            _allowRemoteProvider = allowRemoteProvider;
+        }
+
+        public bool AllowUser(IHttpContext context)
+        {
+            return _allowRemoteProvider.AllowRemote || context.Request.IsLocal;
+        }
+    }
+}
