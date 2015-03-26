afa31ac8b25d9ed3fac1d5531cb83c897e876b4f
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-26 12:39:40 -0700

    Add option for allowed user roles

--- src/Glimpse.Server.Web/GlimpseServerWebOptions.cs
+++ src/Glimpse.Server.Web/GlimpseServerWebOptions.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Glimpse.Server
 {
     public class GlimpseServerWebOptions
     {
+        public GlimpseServerWebOptions()
+        {
+            AllowedUserRoles = new List<string>();
+        }
+
         public bool AllowRemote { get; set; }
+
+        public IList<string> AllowedUserRoles { get; }
     }
 }
