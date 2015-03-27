7f9d1a4dff0b6a3da10d857c92489859df99267a
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-27 15:16:39 -0700

    Add Request Authorizer to master request runtime

--- src/Glimpse.Web.Common/Framework/MasterRequestRuntime.cs
+++ src/Glimpse.Web.Common/Framework/MasterRequestRuntime.cs
@@ -9,15 +9,33 @@ namespace Glimpse.Web
     {
         private readonly IDiscoverableCollection<IRequestRuntime> _requestRuntimes;
         private readonly IDiscoverableCollection<IRequestHandler> _requestHandlers;
+        private readonly IDiscoverableCollection<IRequestAuthorizer> _requestAuthorizers;
 
         public MasterRequestRuntime(IServiceProvider serviceProvider)
         {
             // TODO: Switch these over to being injected and doing the serviceProvider resolve in the middleware
+            _requestAuthorizers = serviceProvider.GetService<IDiscoverableCollection<IRequestAuthorizer>>();
+            _requestAuthorizers.Discover();
+
             _requestRuntimes = serviceProvider.GetService<IDiscoverableCollection<IRequestRuntime>>();
             _requestRuntimes.Discover();
 
             _requestHandlers = serviceProvider.GetService<IDiscoverableCollection<IRequestHandler>>();
             _requestHandlers.Discover();
+        }
+
+        public bool Authorized(IHttpContext context)
+        {
+            foreach (var requestAuthorizer in _requestAuthorizers)
+            {
+                var allowed = requestAuthorizer.AllowUser(context);
+                if (!allowed)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public async Task Begin(IHttpContext context)
