29518d60fdb183daa0104bd9e7ad88ed761bb31e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-03-27 10:47:30 -0700

    Revert "Add option for allowed user roles"
    This reverts commit afa31ac8b25d9ed3fac1d5531cb83c897e876b4f.

--- src/Glimpse.Server.Web/GlimpseServerWebOptions.cs
+++ src/Glimpse.Server.Web/GlimpseServerWebOptions.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Glimpse.Server
 {
     public class GlimpseServerWebOptions
     {
-        public GlimpseServerWebOptions()
-        {
-            AllowedUserRoles = new List<string>();
-        }
-
         public bool AllowRemote { get; set; }
-
-        public IList<string> AllowedUserRoles { get; }
     }
 }
