cad3e1d533b005c1244529c211981482af6c403a
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-04-01 09:59:34 -0700

    Add option for fixed provider

---
+++ src/Glimpse.Web.Common/Framework/FixedRequestAuthorizerProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Glimpse.Web
+{
+    public class FixedRequestAuthorizerProvider : IRequestAuthorizerProvider
+    {
+        public FixedRequestAuthorizerProvider()
+            : this(Enumerable.Empty<IRequestAuthorizer>())
+        {
+        }
+
+        public FixedRequestAuthorizerProvider(IEnumerable<IRequestAuthorizer> controllerTypes)
+        {
+            Policies = new List<IRequestAuthorizer>(controllerTypes);
+        }
+        
+        public IList<IRequestAuthorizer> Policies { get; }
+
+        IEnumerable<IRequestAuthorizer> IRequestAuthorizerProvider.Authorizers => Policies;
+    }
+}
