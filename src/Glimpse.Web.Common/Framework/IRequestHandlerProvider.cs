756d018f4495841c435fb0af9a13376a135bebca
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-04-01 14:20:30 -0700

    Add Provider for RequestHandling

---
+++ src/Glimpse.Web.Common/Framework/IRequestHandlerProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Glimpse.Web
+{
+    public interface IRequestHandlerProvider
+    {
+        IEnumerable<IRequestHandler> Handlers { get; }
+    }
+}
