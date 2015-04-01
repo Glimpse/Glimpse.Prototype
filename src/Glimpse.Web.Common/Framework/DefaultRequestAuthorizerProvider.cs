2e6c9c49ca1f2af49c9fd0dd3e2a964f898b5e92
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-04-01 09:59:23 -0700

    Add default provider which uses reflection

---
+++ src/Glimpse.Web.Common/Framework/DefaultRequestAuthorizerProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+
+namespace Glimpse.Web
+{
+    public class DefaultRequestAuthorizerProvider : IRequestAuthorizerProvider
+    {
+        private readonly ITypeService _typeService;
+
+        public DefaultRequestAuthorizerProvider(ITypeService typeService)
+        {
+            _typeService = typeService;
+        }
+
+        public IEnumerable<IRequestAuthorizer> Authorizers
+        {
+            get { return _typeService.Resolve<IRequestAuthorizer>().ToArray(); }
+        }
+    }
+}
