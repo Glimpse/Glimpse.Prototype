d0939091a5579e00a3adfb8af06b8a60052b2503
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-04-23 15:28:26 -0700

    Add Provider for RequestRuntime

---
+++ src/Glimpse.Web.Common/Framework/IRequestRuntimeProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Glimpse.Web
+{
+    public interface IRequestRuntimeProvider
+    {
+        IEnumerable<IRequestRuntime> Runtimes { get; }
+    }
+}
