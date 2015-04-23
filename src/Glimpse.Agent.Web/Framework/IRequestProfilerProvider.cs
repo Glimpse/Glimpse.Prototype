a14b4179ee3dab0037aad36b899b38150a13aa48
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-04-23 16:00:03 -0700

    Add provider for RequestProfilers

---
+++ src/Glimpse.Agent.Web/Framework/IRequestProfilerProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Glimpse.Agent.Web
+{
+    public interface IRequestProfilerProvider
+    {
+        IEnumerable<IRequestProfiler> Profilers { get; }
+    }
+}
