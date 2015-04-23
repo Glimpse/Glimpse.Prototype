3b3300b08d5ab7fea4c9137d6f5fa5985ffaf08e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-04-23 16:00:24 -0700

    Add default implementation for RequestProfilers provider

---
+++ src/Glimpse.Agent.Web/Framework/DefaultRequestProfilerProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+
+namespace Glimpse.Agent.Web
+{
+    public class DefaultRequestProfilerProvider : IRequestProfilerProvider
+    {
+        private readonly ITypeService _typeService;
+
+        public DefaultRequestProfilerProvider(ITypeService typeService)
+        {
+            _typeService = typeService;
+        }
+
+        public IEnumerable<IRequestProfiler> Profilers
+        {
+            get { return _typeService.Resolve<IRequestProfiler>().ToArray(); }
+        }
+    }
+}
