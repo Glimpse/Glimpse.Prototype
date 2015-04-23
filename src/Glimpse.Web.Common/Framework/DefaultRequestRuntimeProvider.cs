72ef3c6427137ecf56f40c1abfd0ba6663174f23
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-04-23 15:29:15 -0700

    Add default provider for RequestRuntime

---
+++ src/Glimpse.Web.Common/Framework/DefaultRequestRuntimeProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Glimpse.Web
+{
+    public class DefaultRequestRuntimeProvider : IRequestRuntimeProvider
+    {
+        private readonly ITypeService _typeService;
+
+        public DefaultRequestRuntimeProvider(ITypeService typeService)
+        {
+            _typeService = typeService;
+        }
+
+        public IEnumerable<IRequestRuntime> Runtimes
+        {
+            get { return _typeService.Resolve<IRequestRuntime>().ToArray(); }
+        }
+    }
+}
