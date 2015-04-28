f81d82194990985b372e3b793f0481b74d000e4a
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-04-28 10:36:03 -0700

    Refactor AgentBus constructor arg

--- src/Glimpse.Agent.Common/Broker/DefaultMessageAgentBus.cs
+++ src/Glimpse.Agent.Common/Broker/DefaultMessageAgentBus.cs
@@ -14,12 +14,12 @@ namespace Glimpse.Agent
         // TODO: Review if we care about unifying which thread message is published on
         //       and which thread it is recieved on. If so need to use IScheduler.
 
-        public DefaultAgentBroker(IChannelSender currentMessagePublisher)
+        public DefaultAgentBroker(IChannelSender channelSender)
         {
             _subject = new BehaviorSubject<IMessage>(null);
 
             // TODO: This probably shouldn't be here but don't want to setup more infrasture atm
-            ListenAll().Subscribe(async msg => await currentMessagePublisher.PublishMessage(msg));
+            ListenAll().Subscribe(async msg => await channelSender.PublishMessage(msg));
         }
 
         public IObservable<T> Listen<T>()
