9661d337f11031c5428d61c1d28e817f71703660
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-04-30 10:57:32 -0700

    Setup client agent so that it can generate guids

--- src/Glimpse.Agent.Browser/Resources/Embed/scripts/BrowserAgent.js
+++ src/Glimpse.Agent.Browser/Resources/Embed/scripts/BrowserAgent.js
@@ -4,13 +4,18 @@
                 getRequestId: function() {
                     // TODO: Get the current request id somehow
                     return $('#glimpse').attr('data-glimpse-id');
+                },
+                getGuid: function() {
+                    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                            return v.toString(16);
+                        });
                 }
             };
         })(),
         publishMessage = (function() {
-            var getMessageId = function() {
-                    // TODO: Generate random id for this message
-                    return "e3599391-639a-4809-b7d3-10b286f59d3e";
+            var getMessageId = function() { 
+                    return common.getGuid();
                 },
                 getCurrentTime = function() {
                     return new Date();
