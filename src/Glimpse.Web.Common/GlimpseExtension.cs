1211560eb9fd1bb3e04f9911e78eb1470d1d805e
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-08-15 10:05:42 -0700

    Bring logic middleware inside the map and remove map

--- src/Glimpse.Web.Common/GlimpseExtension.cs
+++ src/Glimpse.Web.Common/GlimpseExtension.cs
@@ -23,83 +23,21 @@ namespace Microsoft.AspNet.Builder
             
             // create new pipeline
             var branchBuilder = app.New();
-            // run through logic
-            foreach (var middlewareLogic in middlewareLogicComposers)
+            branchBuilder.Map("/glimpse", innerApp =>
             {
-                middlewareLogic.Register(branchBuilder);
-            }
-            // run through resource
-            branchBuilder.MapUse("/glimpse", innerApp =>
-            {
+                // run through logic
+                foreach (var middlewareLogic in middlewareLogicComposers)
+                {
+                    middlewareLogic.Register(innerApp);
+                }
+                // run through resource
                 foreach (var middlewareResource in middlewareResourceComposers)
                 {
                     middlewareResource.Register(innerApp);
                 }
-            });
+            }); 
 
             return app.Use(next => new GlimpseMiddleware(next, branchBuilder, shouldRun).Invoke);
         }
-    }
-
-
-
-
-
-
-    public static class MapUseExtensions
-    {
-        public static IApplicationBuilder MapUse(this IApplicationBuilder app, PathString pathMatch, Action<IApplicationBuilder> configuration)
-        {
-            if (pathMatch.HasValue && pathMatch.Value.EndsWith("/", StringComparison.Ordinal))
-            {
-                throw new ArgumentException("The path must not end with a '/'", nameof(pathMatch));
-            }
-
-            // create branch
-            var branchBuilder = app.New();
-            configuration(branchBuilder);
-
-            return app.Use(next => new MapUseMiddleware(next, branchBuilder, pathMatch).Invoke);
-        }
-    }
-
-    public class MapUseMiddleware
-    {
-        private readonly RequestDelegate _next;
-        private readonly PathString _pathMatch;
-        private readonly RequestDelegate _branch;
-
-        public MapUseMiddleware(RequestDelegate next, IApplicationBuilder branchBuilder, PathString pathMatch)
-        {
-            _next = next;
-            _pathMatch = pathMatch;
-
-            // this is registered at the end of the pipeline
-            branchBuilder.Use(subNext => { return async ctx => await next(ctx); });
-
-            _branch = branchBuilder.Build();
-        }
-
-        public async Task Invoke(HttpContext context)
-        {
-            PathString path = context.Request.Path;
-            PathString remainingPath;
-            if (path.StartsWithSegments(_pathMatch, out remainingPath))
-            {
-                // Update the path
-                PathString pathBase = context.Request.PathBase;
-                context.Request.PathBase = pathBase + _pathMatch;
-                context.Request.Path = remainingPath;
-
-                await _branch(context);
-
-                context.Request.PathBase = pathBase;
-                context.Request.Path = path;
-            }
-            else
-            {
-                await _next(context);
-            }
-        }
-    }
+    } 
 }
