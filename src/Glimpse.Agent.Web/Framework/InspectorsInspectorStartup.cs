c5324b4dc39173bd97437e16ae51072f912400a4
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-08-27 15:07:11 -0700

    Create implementation for Inspectors InspectorStartup

---
+++ src/Glimpse.Agent.Web/Framework/InspectorsInspectorStartup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Glimpse.Agent.Web
+{
+    public class InspectorsInspectorStartup : IInspectorStartup
+    {
+        private readonly IEnumerable<IInspector> _inspectors;
+
+        public InspectorsInspectorStartup(IInspectorProvider inspectorProvider)
+        {
+            _inspectors = inspectorProvider.Inspectors;
+        }
+
+        public void Configure(IInspectorBuilder builder)
+        {
+            builder.Use(next => async context =>
+            {
+                foreach (var inspector in _inspectors)
+                {
+                    await inspector.Before(context);
+                }
+
+                await next(context);
+
+                foreach (var inspector in _inspectors)
+                {
+                    await inspector.After(context);
+                }
+            });
+        }
+    }
+}
