77ede4b8d33b33e1d140ba6f603e8cd9ee328e06
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-08-31 12:42:35 -0700

    Initial cut of extension classes that will makeup the foundation

---
+++ src/Glimpse.Common/Common/FixedExtensionProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Glimpse
+{
+    public class FixedExtensionProvider<T> : IExtensionProvider<T>
+        where T : class
+    {
+        public FixedExtensionProvider()
+            : this(Enumerable.Empty<T>())
+        {
+        }
+
+        public FixedExtensionProvider(IEnumerable<T> extensionTypes)
+        {
+            Instances = new List<T>(extensionTypes);
+        }
+
+        public IList<T> Instances { get; }
+
+        IEnumerable<T> IExtensionProvider<T>.Instances => Instances;
+    }
+}
