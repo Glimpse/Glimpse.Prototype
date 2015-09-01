b37ef8a3a0c9c0758f0a9e04524bd069b54fe756
Nik Molnar <nikmd23@gmail.com>
2015-09-01 15:23:28 -0400

    Add UserInspector

---
+++ src/Glimpse.Agent.Web/Inspectors/UserInspector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+
+namespace Glimpse.Agent.Web.Inspectors
+{
+    public class UserInspector : IInspector
+    {
+        public Task Before(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task After(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
