8b537c0468a990af7b8a293e5d447f6ef3531a93
Nik Molnar <nikmd23@gmail.com>
2015-09-02 17:37:00 -0400

    Add Inspector base class

---
+++ src/Glimpse.Agent.Web/Framework/Inspector.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Http;
+
+namespace Glimpse.Agent.Web.Framework
+{
+    public class Inspector : IInspector
+    {
+        public virtual void Before(HttpContext context)
+        {
+        }
+
+        public virtual void After(HttpContext context)
+        {
+        }
+    }
+}
