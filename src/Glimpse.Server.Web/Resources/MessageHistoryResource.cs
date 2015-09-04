9a08c25609cef05db7b375403a0b1e289e9fd638
Nik Molnar <nikmd23@gmail.com>
2015-09-04 10:24:15 -0400

    Update resource to handle message strings

--- src/Glimpse.Server.Web/Resources/MessageHistoryResource.cs
+++ src/Glimpse.Server.Web/Resources/MessageHistoryResource.cs
@@ -1,25 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Http;
 using Microsoft.Net.Http.Headers;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace Glimpse.Server.Web
 {
     public class MessageHistoryResource : IResource
     {
         private readonly InMemoryStorage _store;
-        private readonly JsonSerializer _jsonSerializer;
 
-        public MessageHistoryResource(IStorage storage, JsonSerializer jsonSerializer)
+        public MessageHistoryResource(IStorage storage)
         {
-            // TODO: This hack is needed to get around signalr problem
-            jsonSerializer.ContractResolver = new CamelCasePropertyNamesContractResolver();
-
             // TODO: Really shouldn't be here 
             _store = (InMemoryStorage)storage;
-            _jsonSerializer = jsonSerializer;
         }
 
         public async Task Invoke(HttpContext context, IDictionary<string, string> parameters)
@@ -28,8 +22,12 @@ namespace Glimpse.Server.Web
 
             response.Headers[HeaderNames.ContentType] = "application/json";
 
-            var list = _store.Query(null);
-            var output = _jsonSerializer.Serialize(list);
+            var list = await _store.Query(null);
+
+            var sb = new StringBuilder("[");
+            sb.Append(string.Join(",", list));
+            sb.Append("]");
+            var output = sb.ToString();
 
             await response.WriteAsync(output);
         }
