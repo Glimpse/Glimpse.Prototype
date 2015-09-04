3c51fd34394e57d806896904a9eb5c3fd46fa32a
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-09-04 16:13:14 -0700

    Put hook point back into server broker #39

--- src/Glimpse.Server.Web/Broker/DefaultServerBroker.cs
+++ src/Glimpse.Server.Web/Broker/DefaultServerBroker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 
@@ -7,51 +8,39 @@ namespace Glimpse.Server.Web
 {
     public class DefaultServerBroker : IServerBroker
     {
-        //private readonly ISubject<MessageListenerPayload> _subject;
+        private readonly ISubject<IMessage> _offRecieverThreadSubject;
+        private readonly ISubject<IMessage> _offRecieverThreadInternalSubject;
         private readonly IClientBroker _currentMessagePublisher;
         private readonly IStorage _storage;
-
-        // TODO: Review if we care about unifying which thread message is published on
-        //       and which thread it is recieved on. If so need to use IScheduler.
-
-        // TODO: Review how we think people will want to filter on these messages given 
-        //       the lack of structure 
-
+        
         public DefaultServerBroker(IClientBroker currentMessagePublisher, IStorage storage)
         {
-            // TODO: Should add back in at some point?
-            //_subject = new BehaviorSubject<MessageListenerPayload>(null);
             _currentMessagePublisher = currentMessagePublisher;
             _storage = storage;
 
-            //// TODO: This probably shouldn't be here but don't want to setup more infrasture atm
-            //ListenAll().Subscribe(async msg => {
-            //    await currentMessagePublisher.PublishMessage(msg);
-            //    await storage.Persist(msg);
-            //});
+            _offRecieverThreadSubject = new Subject<IMessage>();
+            _offRecieverThreadInternalSubject = new Subject<IMessage>();
+
+            OffRecieverThread = new ServerBrokerHook(_offRecieverThreadSubject);
+
+            // ensure off-request data is observed onto a different thread
+            _offRecieverThreadInternalSubject.Subscribe(payload => Observable.Start(() => _offRecieverThreadSubject.OnNext(payload), TaskPoolScheduler.Default));
         }
+
+        /// <summary>
+        /// Off the reciever thread and is not blocking
+        /// </summary>
+        public ServerBrokerHook OffRecieverThread { get; }
         
-        //public IObservable<MessageListenerPayload> ListenAll()
-        //{
-        //    return ListenAllIncludeLatest().Skip(1);
-        //}
-
-        //public IObservable<MessageListenerPayload> ListenAllIncludeLatest()
-        //{
-        //    return _subject;
-        //}
-
         public void SendMessage(IMessage message)
         {
-            //var notificationOptions = new MessageListenerPayload(message);
+            // non-blocking
+            _offRecieverThreadInternalSubject.OnNext(message);
 
-            //_subject.OnNext(notificationOptions);
-
-            //if (!notificationOptions.IsCancelled)
-            //{
-                _currentMessagePublisher.PublishMessage(message);
-                _storage.Persist(message);
-            //}
+            // TODO: This could probably be altered to be a listener of OffRecieverThread
+            // blocking
+            _currentMessagePublisher.PublishMessage(message);
+            _storage.Persist(message); 
         }
     }
 }
