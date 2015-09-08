b24e859a61313853fb742ea20187422f07be1741
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-09-07 20:50:21 -0700

    Update options class with better name

--- src/Glimpse.Server.Web/Framework/AuthorizeClientOptionsCanAccessClient.cs
+++ src/Glimpse.Server.Web/Framework/AuthorizeClientOptionsCanAccessClient.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.OptionsModel;
 
 namespace Glimpse.Server.Web.Framework
 {
-    public class AuthorizeClientOptionsCanAccess : IAuthorizeClient
+    public class AuthorizeClientOptionsCanAccessClient : IAuthorizeClient
     {
         private readonly Func<HttpContext, bool> _canAccess;
 
-        public AuthorizeClientOptionsCanAccess(IOptions<GlimpseServerWebOptions> optionsAccessor)
+        public AuthorizeClientOptionsCanAccessClient(IOptions<GlimpseServerWebOptions> optionsAccessor)
         {
             _canAccess = optionsAccessor.Value.CanAccessClient;
         }
