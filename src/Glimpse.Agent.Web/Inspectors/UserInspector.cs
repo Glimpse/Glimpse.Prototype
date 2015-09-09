0d27b1e78f4c845babc29c28e407a71bb8b2270b
Nik Molnar <nikmd23@gmail.com>
2015-09-09 12:55:03 -0400

    Add caching to avoid unnecessary hashing.

--- src/Glimpse.Agent.Web/Inspectors/UserInspector.cs
+++ src/Glimpse.Agent.Web/Inspectors/UserInspector.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using Glimpse.Agent.Web.Framework;
 using Glimpse.Agent.Web.Message;
 using Microsoft.AspNet.Http;
@@ -9,7 +10,9 @@ namespace Glimpse.Agent.Web.Inspectors
     public class UserInspector : Inspector
     {
         private readonly IAgentBroker _broker;
-        private const string GlimpseCookie = ".Glimpse.Session";
+        private const string GlimpseCookie = ".Glimpse.Session"; // TODO: Move this somewhere configurable
+        private readonly ConcurrentDictionary<string, string> _gravatarCache = new ConcurrentDictionary<string, string>(); 
+        private readonly ConcurrentDictionary<string, string> _crcCache = new ConcurrentDictionary<string, string>(); 
 
         public UserInspector(IAgentBroker broker)
         {
@@ -33,9 +36,9 @@ namespace Glimpse.Agent.Web.Inspectors
                 }
             }
 
-            var username = user.FindFirst("name")?.Value ?? userId.Crc16().ToUpper();
+            var username = user.FindFirst("name")?.Value ?? _crcCache.GetOrAdd(userId, id => id.Crc16().ToUpper());
             var email = user.FindFirst("email")?.Value;
-            var pic = user.FindFirst("picture")?.Value ?? GenerateGravatarUrl(email ?? userId);
+            var pic = user.FindFirst("picture")?.Value ?? _gravatarCache.GetOrAdd(email ?? userId, GenerateGravatarUrl);
 
             _broker.SendMessage(new UserIdentification(userId, username, email, pic));
         }
