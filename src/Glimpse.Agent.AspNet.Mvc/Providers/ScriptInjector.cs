c9c692861bb09562a09ebb56fa967465e6435ace
Nik Molnar <nikmd23@gmail.com>
2015-09-14 18:26:00 -0700

    Inject request Id into data-attribute

--- src/Glimpse.Agent.AspNet.Mvc/Providers/ScriptInjector.cs
+++ src/Glimpse.Agent.AspNet.Mvc/Providers/ScriptInjector.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text;
 using Microsoft.AspNet.Razor.Runtime.TagHelpers;
 using Microsoft.AspNet.Mvc.Rendering;
@@ -7,21 +8,32 @@ namespace Glimpse.Agent.AspNet.Mvc
     [HtmlTargetElement("body")]
     public class ScriptInjector : TagHelper
     {
+        private readonly Guid _requestId;
+
+        public ScriptInjector(IContextData<MessageContext> context)
+        {
+            _requestId = context.Value.Id;
+        }
+
         public override int Order => int.MaxValue;
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            var js = new StringBuilder();
-            js.AppendLine("var link = document.createElement('a');");
-            js.AppendLine("link.setAttribute('href', '/glimpse/client/index.html');");
-            js.AppendLine("link.setAttribute('target', '_blank');");
-            js.AppendLine("link.text = 'Open Glimpse';");
-            js.AppendLine("document.body.appendChild(link);");
+            var js = @"
+var script = document.querySelector('script[data-request-id]'),
+link = document.createElement('a'),
+requestId = script.dataset.requestId;
+link.setAttribute('href', '/glimpse/client/index.html?id=' + requestId);
+link.setAttribute('target', '_blank');
+link.text = 'Open Glimpse';
+document.body.appendChild(link);";
 
             var tag = new TagBuilder("script")
             {
-                InnerHtml = new HtmlString(js.ToString())
+                InnerHtml = new HtmlString(js)
             };
+
+            tag.Attributes.Add("data-request-id", _requestId.ToString());
 
             output.PostContent.Append(tag);
         }
