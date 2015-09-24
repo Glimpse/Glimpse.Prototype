f6dd66406685e4f27e042ed38b837f7160e82571
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-09-24 00:07:40 -0700

    Fixup spacing in message object

--- src/Glimpse.Agent.AspNet.Mvc/Messages/RouteData.cs
+++ src/Glimpse.Agent.AspNet.Mvc/Messages/RouteData.cs
@@ -6,7 +6,9 @@ namespace Glimpse.Agent.AspNet.Mvc.Messages
     public class RouteData
     {
         public string Name { get; set; }
+
         public string Pattern { get; set; }
+
         public IList<RouteResolutionData> Data { get; set; }
     }
 }
