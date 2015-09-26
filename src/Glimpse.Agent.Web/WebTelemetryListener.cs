9480f273d40d83aa6b00da59184f58b35a32a87a
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-09-26 08:33:20 -0700

    Add back in the implementation for tracking begin and end request

--- src/Glimpse.Agent.Web/WebTelemetryListener.cs
+++ src/Glimpse.Agent.Web/WebTelemetryListener.cs
@@ -25,16 +25,16 @@ namespace Glimpse.Agent.Web
             // TODO: Not sure if this is where this should live but it's the earlist hook point we have
             _contextData.Value = new MessageContext { Id = Guid.NewGuid(), Type = "Request" };
 
-            //var request = httpContext.Request;
+            var request = httpContext.Request;
 
-            //var beginMessage = new BeginRequestMessage
-            //{
-            //    // TODO: check if there is a better way of doing this
-            //    // TODO: should there be a StartTime property here?
-            //    Url = $"{request.Scheme}://{request.Host}{request.PathBase}{request.Path}{request.QueryString}"
-            //};
+            var beginMessage = new BeginRequestMessage
+            {
+                // TODO: check if there is a better way of doing this
+                // TODO: should there be a StartTime property here?
+                Url = $"{request.Scheme}://{request.Host}{request.PathBase}{request.Path}{request.QueryString}"
+            };
 
-            //_broker.BeginLogicalOperation(beginMessage);
+            _broker.BeginLogicalOperation(beginMessage);
         }
 
         [TelemetryName("Microsoft.AspNet.Hosting.EndRequest")]
@@ -42,22 +42,22 @@ namespace Glimpse.Agent.Web
         {
             var timing = _broker.EndLogicalOperation<BeginRequestMessage>().Timing;
 
-            //var request = httpContext.Request;
-            //var response = httpContext.Response;
+            var request = httpContext.Request;
+            var response = httpContext.Response;
 
-            //var endMessage = new EndRequestMessage
-            //{
-            //    // TODO: check if there is a better way of doing this
-            //    Url = $"{request.Scheme}://{request.Host}{request.PathBase}{request.Path}{request.QueryString}",
-            //    Duration = timing.Elapsed.TotalMilliseconds,
-            //    Method = request.Method,
-            //    ContentType = response.ContentType,
-            //    StatusCode = response.StatusCode,
-            //    StartTime = timing.Start.ToUniversalTime(),
-            //    EndTime = timing.End.ToUniversalTime()
-            //};
+            var endMessage = new EndRequestMessage
+            {
+                // TODO: check if there is a better way of doing this
+                Url = $"{request.Scheme}://{request.Host}{request.PathBase}{request.Path}{request.QueryString}",
+                Duration = timing.Elapsed.TotalMilliseconds,
+                Method = request.Method,
+                ContentType = response.ContentType,
+                StatusCode = response.StatusCode,
+                StartTime = timing.Start.ToUniversalTime(),
+                EndTime = timing.End.ToUniversalTime()
+            };
 
-            //_broker.SendMessage(endMessage);
+            _broker.SendMessage(endMessage);
         }
     }
 }
