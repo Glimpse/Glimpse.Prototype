b2cd9835c157faa07c21ae116caf72168f573003
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-09-30 13:01:24 -0700

    Make sure that the ViewResultFoundStatusMessage implements correct interface

--- src/Glimpse.Agent.Web/Providers/Mvc/Messages/ViewResultFoundStatusMessage.cs
+++ src/Glimpse.Agent.Web/Providers/Mvc/Messages/ViewResultFoundStatusMessage.cs
@@ -2,7 +2,7 @@
 
 namespace Glimpse.Agent.AspNet.Mvc.Messages
 {
-    public class ViewResultFoundStatusMessage
+    public class ViewResultFoundStatusMessage : IActionViewMessage
     {
         public string ActionId { get; set; }
 
