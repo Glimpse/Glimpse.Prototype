4293b1f599055cc26c9f2bbf5dc25f6eeb951129
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-10-01 14:08:54 -0700

    Add IActionMessage to the messages being published

--- src/Glimpse.Agent.Web/Providers/Mvc/Messages/AfterActionInvokedMessage.cs
+++ src/Glimpse.Agent.Web/Providers/Mvc/Messages/AfterActionInvokedMessage.cs
@@ -1,9 +1,15 @@
 ﻿namespace Glimpse.Agent.AspNet.Mvc.Messages
 {
-    public class AfterActionInvokedMessage
+    public class AfterActionInvokedMessage : IActionMessage
     {
         public string ActionId { get; set; }
 
+        public string DisplayName { get; set; }
+
+        public string ActionName { get; set; }
+
+        public string ControllerName { get; set; }
+        
         public Timing Timing { get; set; }
     }
 }
---
+++ src/Glimpse.Agent.Web/Providers/Mvc/Messages/IActionMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Glimpse.Agent.AspNet.Mvc.Messages
+{
+    public interface IActionMessage
+    {
+        string ActionId { get; set; }
+
+        string DisplayName { get; set; }
+
+        string ActionName { get; set; }
+
+        string ControllerName { get; set; }
+
+        Timing Timing { get; set; }
+    }
+}
