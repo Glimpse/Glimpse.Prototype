007f05b2200277378bfcd502dab802d3531d7d81
Nik Molnar <nikmd23@gmail.com>
2015-10-01 13:45:27 -0500

    Refactored KebabCase code

---
+++ src/Glimpse.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Glimpse.Common
+{
+    public static class StringExtensions
+    {
+        private readonly static Regex _regex = new Regex("([A-Z0-9])", RegexOptions.Compiled);
+
+        public static string KebabCase(this string input)
+        {
+            return _regex.Replace(input, Replacement);
+        }
+
+        private static string Replacement(Match m)
+        {
+            var result = m.Value.ToLower();
+            return m.Index == 0 ? result : "-" + result;
+        }
+    }
+}
