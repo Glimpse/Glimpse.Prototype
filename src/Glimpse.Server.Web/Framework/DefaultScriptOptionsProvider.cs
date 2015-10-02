c6c1412aef06be1b24dfe96f7b682754c30049f0
Nik Molnar <nikmd23@gmail.com>
2015-10-02 17:56:52 -0500

    Resolve Uri Templates in ScriptOptions

--- src/Glimpse.Server.Web/Framework/DefaultScriptOptionsProvider.cs
+++ src/Glimpse.Server.Web/Framework/DefaultScriptOptionsProvider.cs
@@ -1,4 +1,6 @@
-﻿using Glimpse.Web;
+﻿using System.Collections.Generic;
+using Glimpse.Web;
+using Tavis.UriTemplates;
 
 namespace Glimpse.Server.Web
 {
@@ -14,13 +16,26 @@ namespace Glimpse.Server.Web
         public ScriptOptions BuildInstance()
         {
             var metadata = _metadataProvider.BuildInstance();
+            var supportedParameters = new Dictionary<string, object> {{"hash", metadata.Hash}};
+
+            var browserAgentScriptTemplate = new UriTemplate(metadata.Resources.GetValueOrDefault("browser-agent-script") ?? "");
+            browserAgentScriptTemplate.AddParameters(supportedParameters);
+
+            var httpMessageTemplate = new UriTemplate(metadata.Resources.GetValueOrDefault("agent-message") ?? "");
+            httpMessageTemplate.AddParameters(supportedParameters);
+
+            var hudClientScriptTemplate = new UriTemplate(metadata.Resources.GetValueOrDefault("hud-client-script") ?? "");
+            hudClientScriptTemplate.AddParameters(supportedParameters);
+
+            var metadataTemplate = new UriTemplate(metadata.Resources.GetValueOrDefault("metadata") ?? "");
+            metadataTemplate.AddParameters(supportedParameters);
 
             return new ScriptOptions
             {
-                BrowserAgentScriptUri = metadata.Resources.GetValueOrDefault("browser-agent-script"),
-                HttpMessageUri = metadata.Resources.GetValueOrDefault("agent-message"),
-                HudClientScriptUri = metadata.Resources.GetValueOrDefault("hud-client-script"),
-                MetadataUri = metadata.Resources.GetValueOrDefault("metadata")
+                BrowserAgentScriptUri = browserAgentScriptTemplate.Resolve(),
+                HttpMessageUri = httpMessageTemplate.Resolve(),
+                HudClientScriptUri = hudClientScriptTemplate.Resolve(),
+                MetadataUri = metadataTemplate.Resolve()
             };
         }
     }
