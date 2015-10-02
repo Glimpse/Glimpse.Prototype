8360dce3b9e6b54ac7e3dfdbdf4e0ae519254989
Nik Molnar <nikmd23@gmail.com>
2015-10-02 14:44:45 -0500

    Implement Metadata.Hash with Crc32

--- src/Glimpse.Server.Web/Framework/Metadata.cs
+++ src/Glimpse.Server.Web/Framework/Metadata.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Glimpse.Server.Web
 {
@@ -12,6 +13,9 @@ namespace Glimpse.Server.Web
 
         public IReadOnlyDictionary<string, string> Resources { get; set; }
 
-        public string Hash { get; set; }
+        public string Hash
+        {
+            get { return string.Join("&", Resources.Select(r => $"{r.Key}={r.Value}")).Crc32(); }
+        }
     }
 }
