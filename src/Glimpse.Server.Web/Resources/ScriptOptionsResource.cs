a026bbbf0cf36161482e0470860cce05627c2a07
Nik Molnar <nikmd23@gmail.com>
2015-10-02 15:15:11 -0500

    Hand rolled serialization since I always want indented formatting for this human facing JSON.

--- src/Glimpse.Server.Web/Resources/ScriptOptionsResource.cs
+++ src/Glimpse.Server.Web/Resources/ScriptOptionsResource.cs
@@ -3,31 +3,32 @@ using System.Threading.Tasks;
 using Glimpse.Web;
 using Microsoft.AspNet.Http;
 using Microsoft.Net.Http.Headers;
-using Newtonsoft.Json;
 
 namespace Glimpse.Server.Web.Resources
 {
     public class ScriptOptionsResource : IResource
     {
         private readonly IScriptOptionsProvider _scriptOptionsProvider;
-        private readonly JsonSerializer _serializer;
 
-        public ScriptOptionsResource(IScriptOptionsProvider scriptOptionsProvider, JsonSerializer serializer)
+        public ScriptOptionsResource(IScriptOptionsProvider scriptOptionsProvider)
         {
             _scriptOptionsProvider = scriptOptionsProvider;
-            _serializer = serializer;
         }
 
         public async Task Invoke(HttpContext context, IDictionary<string, string> parameters)
         {
             var scriptOptions = _scriptOptionsProvider.BuildInstance();
-            _serializer.Formatting = Formatting.Indented;
-            var result = _serializer.Serialize(scriptOptions);
+            var json = $@"{{
+    ""browserAgentScriptUri"" : ""{scriptOptions.BrowserAgentScriptUri}"",
+    ""hudClientScriptUri"" : ""{scriptOptions.HudClientScriptUri}"",
+    ""httpMessageUri"" : ""{scriptOptions.HttpMessageUri}"",
+    ""metadataUri"" : ""{scriptOptions.MetadataUri}""
+}}";
 
             var response = context.Response;
             response.Headers[HeaderNames.ContentType] = "application/json";
             response.Headers[HeaderNames.ContentDisposition] = "attachment; filename = glimpse.json";
-            await response.WriteAsync(result);
+            await response.WriteAsync(json);
         }
 
         public string Name => "ScriptOptions";
