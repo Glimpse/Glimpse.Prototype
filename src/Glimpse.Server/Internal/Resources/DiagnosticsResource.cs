c55b6fa9c17972a7976427140831b3a52f788659
Nik Molnar <nikmd23@gmail.com>
2015-10-07 16:37:37 -0500

    Refactor Glimpse.Client.Web.Simple into an IResource in Glimpse.Server at /glimpse/Diagnostics/index.html

---
+++ src/Glimpse.Server/Internal/Resources/DiagnosticsResource.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using Glimpse.Server.Resources;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.FileProviders;
+using Microsoft.AspNet.StaticFiles;
+
+namespace Glimpse.Server.Internal.Resources
+{
+    public class DiagnosticsResource : IResourceStartup
+    {
+        public void Configure(IResourceBuilder resourceBuilder)
+        {
+            var options = new FileServerOptions
+            {
+                RequestPath = "/Diagnostics",
+                EnableDefaultFiles = false
+            };
+            options.StaticFileOptions.ContentTypeProvider = new FileExtensionContentTypeProvider();
+            options.FileProvider = new EmbeddedFileProvider(GetType().GetTypeInfo().Assembly, "Glimpse.Server.Internal.Resources.Embeded.Diagnostics");
+
+            resourceBuilder.AppBuilder.UseFileServer(options);
+        }
+
+        public ResourceType Type => ResourceType.Client;
+    }
+}
