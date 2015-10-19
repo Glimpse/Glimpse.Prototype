3b7bb08b90ad5d6aa35d1bbb9f8f83def2c8df22
Nik Molnar <nikmd23@gmail.com>
2015-10-19 17:56:42 -0500

    Add environment inspector

---
+++ src/Glimpse.Agent.AspNet/Internal/Inspectors/AspNet/EnvironmentInspector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Glimpse.Agent.AspNet.Messages;
+using Glimpse.Agent.Inspectors;
+using Microsoft.AspNet.Http;
+
+namespace Glimpse.Agent.AspNet.Internal.Inspectors.AspNet
+{
+    public class EnvironmentInspector : Inspector
+    {
+        private readonly IAgentBroker _broker;
+        private EnvironmentMessage _message;
+
+        public EnvironmentInspector(IAgentBroker broker)
+        {
+            _broker = broker;
+        }
+
+        public override void Before(HttpContext context)
+        {
+            if (_message == null)
+            {
+                _message = new EnvironmentMessage
+                {
+                    Server = Environment.MachineName,
+                    OperatingSystem = Environment.OSVersion.VersionString,
+                    ProcessorCount = Environment.ProcessorCount,
+                    Is64Bit = Environment.Is64BitOperatingSystem,
+                    CommandLineArgs = Environment.GetCommandLineArgs(),
+                    EnvironmentVariables = Environment.GetEnvironmentVariables()
+                };
+            }
+
+            _broker.SendMessage(_message);
+        }
+    }
+}
---
+++ src/Glimpse.Agent.AspNet/Messages/EnvironmentMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+namespace Glimpse.Agent.AspNet.Messages
+{
+    //TODO: This is just an example to get an environment message up and running. More thought should be put into this
+    public class EnvironmentMessage
+    {
+        public string Server { get; set; }
+        public string OperatingSystem { get; set; }
+        public int ProcessorCount { get; set; }
+        public bool Is64Bit { get; set; }
+        public string[] CommandLineArgs { get; set; }
+        public IDictionary EnvironmentVariables { get; set; }
+    }
+}
