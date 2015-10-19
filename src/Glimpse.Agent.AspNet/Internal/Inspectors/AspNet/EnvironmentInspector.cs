c5471cc62ebe67750c990c40fbe9bf0e40537259
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-10-19 16:26:41 -0700

    Comment out the environment message for the time being

--- src/Glimpse.Agent.AspNet/Internal/Inspectors/AspNet/EnvironmentInspector.cs
+++ src/Glimpse.Agent.AspNet/Internal/Inspectors/AspNet/EnvironmentInspector.cs
@@ -17,20 +17,20 @@ namespace Glimpse.Agent.AspNet.Internal.Inspectors.AspNet
 
         public override void Before(HttpContext context)
         {
-            if (_message == null)
-            {
-                _message = new EnvironmentMessage
-                {
-                    Server = Environment.MachineName,
-                    OperatingSystem = Environment.OSVersion.VersionString,
-                    ProcessorCount = Environment.ProcessorCount,
-                    Is64Bit = Environment.Is64BitOperatingSystem,
-                    CommandLineArgs = Environment.GetCommandLineArgs(),
-                    EnvironmentVariables = Environment.GetEnvironmentVariables()
-                };
-            }
+            //if (_message == null)
+            //{
+            //    _message = new EnvironmentMessage
+            //    {
+            //        Server = Environment.MachineName,
+            //        OperatingSystem = Environment.OSVersion.VersionString,
+            //        ProcessorCount = Environment.ProcessorCount,
+            //        Is64Bit = Environment.Is64BitOperatingSystem,
+            //        CommandLineArgs = Environment.GetCommandLineArgs(),
+            //        EnvironmentVariables = Environment.GetEnvironmentVariables()
+            //    };
+            //}
 
-            _broker.SendMessage(_message);
+            //_broker.SendMessage(_message);
         }
     }
 }
