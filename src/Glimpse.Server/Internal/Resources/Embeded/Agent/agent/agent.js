0f68625ba245bd804410d997a04066ea3e7a5cb1
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-10-21 15:51:03 -0700

    Update GUID format of Browser Agent

--- src/Glimpse.Server/Internal/Resources/Embeded/Agent/agent/agent.js
+++ src/Glimpse.Server/Internal/Resources/Embeded/Agent/agent/agent.js
@@ -20,7 +20,7 @@
                 return document.getElementById('__glimpse_browser_agent').getAttribute('data-message-ingress-template');
             },
             getGuid: function() {
-                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
                     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
                     return v.toString(16);
                 });
