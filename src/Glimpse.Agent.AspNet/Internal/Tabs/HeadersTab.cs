6db48c62e0ec963591b0900ded92d2446303b1ac
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-11-10 20:47:43 -0800

    Remove tabs for the time being

--- src/Glimpse.Agent.AspNet/Internal/Tabs/HeadersTab.cs
+++ src/Glimpse.Agent.AspNet/Internal/Tabs/HeadersTab.cs
@@ -1,20 +1,20 @@
-﻿using Glimpse.Agent.Inspectors;
-using Microsoft.AspNet.Http;
+﻿//using Glimpse.Agent.Inspectors;
+//using Microsoft.AspNet.Http;
 
-namespace Glimpse.Agent.AspNet.Internal.Tabs
-{
-    // TODO: Delete me. This tab isn't intended to stick around, it's just a sample of a Tab.
-    public class HeadersTab : Tab
-    {
-        public override string Name => "Headers";
+//namespace Glimpse.Agent.AspNet.Internal.Tabs
+//{
+//    // TODO: Delete me. This tab isn't intended to stick around, it's just a sample of a Tab.
+//    public class HeadersTab : Tab
+//    {
+//        public override string Name => "Headers";
 
-        public override object GetData(HttpContext context)
-        {
-            return new
-            {
-                Request = context.Request.Headers,
-                Response = context.Response.Headers
-            };
-        }
-    }
-}
+//        public override object GetData(HttpContext context)
+//        {
+//            return new
+//            {
+//                Request = context.Request.Headers,
+//                Response = context.Response.Headers
+//            };
+//        }
+//    }
+//}
