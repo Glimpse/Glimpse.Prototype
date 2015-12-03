ab6801438ac7a0c5e9ba5d2b011286f98529bb37
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-12-03 11:28:30 -0800

    Create root entry point for SystemWeb

---
+++ src/Glimpse.Common/_SystemWeb/Glimpse.cs
@@ -0,0 +1,23 @@
+﻿#if SystemWeb
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Glimpse
+{
+    public static class Glimpse
+    {
+        public static IServiceCollection Start()
+        {
+            return Start(null);
+        }
+
+        public static IServiceCollection Start(IServiceCollection serviceProvider)
+        {
+            return null;
+        }
+    }
+}
+#endif
