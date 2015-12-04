878602fb47e8f45a32a40d38f4147f18423c4510
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-12-03 21:53:03 -0800

    Make type selection a little more robust given SystemWeb assembly discovery

--- src/Glimpse.Common/Internal/Reflection/DefaultTypeSelector.cs
+++ src/Glimpse.Common/Internal/Reflection/DefaultTypeSelector.cs
@@ -10,13 +10,29 @@ namespace Glimpse.Internal
         public IEnumerable<TypeInfo> FindTypes(IEnumerable<Assembly> targetAssmblies, TypeInfo targetTypeInfo)
         {
             var types = targetAssmblies
-                .SelectMany(a => a.DefinedTypes)
+                .SelectMany(GetLoadableTypes)
                 .Where(t => t.IsClass &&
                         !t.IsAbstract &&
                         !t.ContainsGenericParameters &&
                         targetTypeInfo.IsAssignableFrom(t));
 
             return types;
+        }
+
+        private static IEnumerable<TypeInfo> GetLoadableTypes(Assembly assembly)
+        {
+            try
+            {
+                return assembly.DefinedTypes;
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                return e.Types.Where(t => t != null).Select(t => t.GetTypeInfo());
+            }
+            catch (Exception)
+            {
+                return Enumerable.Empty<TypeInfo>();
+            }
         }
     }
 }
