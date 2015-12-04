79799c4dd8e910e6c372302ac3a499aec1b6e6e3
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-12-04 06:59:50 -0800

    Remove unused namespaces

--- src/Glimpse.Common/_SystemWeb/Glimpse.cs
+++ src/Glimpse.Common/_SystemWeb/Glimpse.cs
@@ -1,8 +1,4 @@
 ﻿#if SystemWeb
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Glimpse
