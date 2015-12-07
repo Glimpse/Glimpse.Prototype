e4f3efeebbdc5d2b148fc89f0436a657d1975ea7
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-12-06 21:08:29 -0800

    Base implementation of HttpHandler

---
+++ src/Glimpse.Server/_SystemWeb/GlimpseServerHandler.cs
@@ -0,0 +1,28 @@
+﻿#if SystemWeb
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using SystemWebAdapter;
+
+namespace Glimpse
+{
+    public class GlimpseServerHandler : IHttpHandler
+    {
+        public bool IsReusable => true;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+        private static Microsoft.AspNet.Http.HttpContext GetHttpContext(object sender)
+        {
+            var httpApplication = (HttpApplication)sender;
+
+            // convert SystemWeb HttpContext to DNX HttpContext 
+            return httpApplication.Context.CreateContext();
+        }
+    }
+}
+#endif
