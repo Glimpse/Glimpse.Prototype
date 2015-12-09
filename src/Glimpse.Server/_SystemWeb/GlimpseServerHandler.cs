df34c906c205138f477b277c8bb59bbfd118adff
Anthony van der Hoorn <anthony.vanderhoorn@gmail.com>
2015-12-08 22:41:52 -0800

    Initial real implementation of Server Handler

--- src/Glimpse.Server/_SystemWeb/GlimpseServerHandler.cs
+++ src/Glimpse.Server/_SystemWeb/GlimpseServerHandler.cs
@@ -1,21 +1,32 @@
 ﻿#if SystemWeb
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Web;
 using SystemWebAdapter;
+using Glimpse.Server.Resources;
 
 namespace Glimpse
 {
     public class GlimpseServerHandler : IHttpHandler
     {
+        private readonly IResourceRuntimeManager _resourceRuntimeManager;
+
+        public GlimpseServerHandler(IResourceRuntimeManager resourceRuntimeManager)
+        {
+            _resourceRuntimeManager = resourceRuntimeManager;
+
+            // TODO: Need to check if this only runs once or multiple times
+            _resourceRuntimeManager.Register();
+        }
+
         public bool IsReusable => true;
 
-        public void ProcessRequest(HttpContext context)
+        public void ProcessRequest(HttpContext httpContext)
         {
-            throw new NotImplementedException();
+            var context = GetHttpContext(httpContext);
+
+            // need to wait as ProcessRequest is sync
+            _resourceRuntimeManager.ProcessRequest(context).Wait();
         }
+
         private static Microsoft.AspNet.Http.HttpContext GetHttpContext(object sender)
         {
             var httpApplication = (HttpApplication)sender;
